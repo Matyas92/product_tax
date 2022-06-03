89442438adfd05c9168b226ac8bbd4541875de9a
Matyas92 <kosa.matyas@gmail.com>
2022-06-03 13:00:04 +0200

    Js-create-DOM

---
+++ app.js
@@ -0,0 +1,7 @@
+
+//Putting the DOM here as the form and the set_body where will be uploaded the datas
+//Final is a tbody in html which writes the all prices with taxes and the diff between all taxes and NET prices 
+const form  = document.getElementById('input');
+set_body = document.querySelector(".set_body")
+set_final = document.querySelector(".final")
+
